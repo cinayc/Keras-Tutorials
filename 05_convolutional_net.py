6403660587026db85ccb8525d362f43d96181753
Jeremy Park <cyanic@gmail.com>
2017-08-31 18:36:22 +0900

    for keras2

--- 05_convolutional_net.py
+++ 05_convolutional_net.py
@@ -1,9 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Convolution2D, MaxPooling2D, Dropout
+from keras.layers import Dense, Flatten, Convolution2D, MaxPooling2D, Dropout, Conv2D
 from keras.optimizers import RMSprop
 from keras.datasets import mnist
 from keras.utils import np_utils
-from keras import initializations
+from keras import initializers
 from keras import backend as K
 
 batch_size = 128
@@ -14,9 +14,6 @@ img_rows, img_cols = 28, 28         # input image dimensions
 pool_size = (2, 2)                  # size of pooling area for max pooling
 prob_drop_conv = 0.2                # drop probability for dropout @ conv layer
 prob_drop_hidden = 0.5              # drop probability for dropout @ fc layer
-
-def init_weights(shape, name=None):
-    return initializations.normal(shape, scale=0.01, name=name)
 
 # Load MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -48,27 +45,27 @@ print(X_test.shape[0], 'test samples')
 model = Sequential()
 
 # conv1 layer
-model.add(Convolution2D(32, 3, 3, border_mode='same', activation='relu', input_shape=input_shape, init=init_weights))
-model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), border_mode='same'))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape, padding='same', kernel_initializer='random_normal'))
+model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), padding='same'))
 model.add(Dropout(prob_drop_conv))
 
 # conv2 layer
-model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu', init=init_weights))
-model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), border_mode='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='random_normal'))
+model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), padding='same'))
 model.add(Dropout(prob_drop_conv))
 
 # conv3 layer
-model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu', init=init_weights))
-model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), border_mode='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same' , kernel_initializer='random_normal'))
+model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), padding='same'))
 model.add(Flatten())
 model.add(Dropout(prob_drop_conv))
 
 # fc1 layer
-model.add(Dense(625, activation='relu', init=init_weights))
+model.add(Dense(625, activation='relu', kernel_initializer='random_normal'))
 model.add(Dropout(prob_drop_hidden))
 
 # fc2 layer
-model.add(Dense(10, activation='softmax', init=init_weights))
+model.add(Dense(10, activation='softmax', kernel_initializer='random_normal'))
 
 opt = RMSprop(lr=0.001, rho=0.9)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
